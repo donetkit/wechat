80007b74874aa3bb1f45665eed7e3feaced96206
donetkit <donetkit@weinre.cn>
2021-04-24 16:45:08 +0800

    微信支付退款请求参数签名类型不可选

--- go.mod
+++ go.mod
@@ -1,15 +1,25 @@
 module github.com/donetkit/wechat
 
-go 1.14
+go 1.16
 
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/fatih/structs v1.1.0
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/gin v1.7.1
+	github.com/go-playground/validator/v10 v10.5.0 // indirect
 	github.com/go-redis/redis/v8 v8.7.1
-	github.com/sirupsen/logrus v1.6.0
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cast v1.3.1
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
-	gopkg.in/h2non/gock.v1 v1.0.15
+	github.com/ugorji/go v1.2.5 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/h2non/gock.v1 v1.0.16
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pay/refund/refund.go
+++ pay/refund/refund.go
@@ -3,7 +3,6 @@ package refund
 import (
 	"encoding/xml"
 	"fmt"
-
 	"github.com/donetkit/wechat/pay/config"
 	"github.com/donetkit/wechat/util"
 )
@@ -21,25 +20,28 @@ func NewRefund(cfg *config.Config) *Refund {
 	return &refund
 }
 
-//Params 调用参数
+// Params 调用参数
 type Params struct {
 	TransactionID string
 	OutRefundNo   string
+	OutTradeNo    string
 	TotalFee      string
 	RefundFee     string
 	RefundDesc    string
-	RootCa        string //ca证书
+	RootCa        string // ca证书
 	NotifyURL     string
+	SignType      string
 }
 
-//request 接口请求参数
+// request 接口请求参数
 type request struct {
 	AppID         string `xml:"appid"`
 	MchID         string `xml:"mch_id"`
 	NonceStr      string `xml:"nonce_str"`
 	Sign          string `xml:"sign"`
 	SignType      string `xml:"sign_type,omitempty"`
-	TransactionID string `xml:"transaction_id"`
+	TransactionID string `xml:"transaction_id,omitempty"`
+	OutTradeNo    string `xml:"out_trade_no,omitempty"`
 	OutRefundNo   string `xml:"out_refund_no"`
 	TotalFee      string `xml:"total_fee"`
 	RefundFee     string `xml:"refund_fee"`
@@ -47,7 +49,7 @@ type request struct {
 	NotifyURL     string `xml:"notify_url,omitempty"`
 }
 
-//Response 接口返回
+// Response 接口返回
 type Response struct {
 	ReturnCode          string `xml:"return_code"`
 	ReturnMsg           string `xml:"return_msg"`
@@ -71,22 +73,9 @@ type Response struct {
 	CashFeeType         string `xml:"cash_fee_type,omitempty"`
 }
 
-//Refund 退款申请
+// Refund 退款申请
 func (refund *Refund) Refund(p *Params) (rsp Response, err error) {
-	nonceStr := util.RandomStr(32)
-	param := make(map[string]string)
-	param["appid"] = refund.AppID
-	param["mch_id"] = refund.MchID
-	param["nonce_str"] = nonceStr
-	param["out_refund_no"] = p.OutRefundNo
-	param["refund_desc"] = p.RefundDesc
-	param["refund_fee"] = p.RefundFee
-	param["total_fee"] = p.TotalFee
-	param["sign_type"] = util.SignTypeMD5
-	param["transaction_id"] = p.TransactionID
-	if p.NotifyURL != "" {
-		param["notify_url"] = p.NotifyURL
-	}
+	param := refund.GetSignParam(p)
 
 	sign, err := util.ParamSign(param, refund.Key)
 	if err != nil {
@@ -94,17 +83,24 @@ func (refund *Refund) Refund(p *Params) (rsp Response, err error) {
 	}
 
 	req := request{
-		AppID:         refund.AppID,
-		MchID:         refund.MchID,
-		NonceStr:      nonceStr,
-		Sign:          sign,
-		SignType:      util.SignTypeMD5,
-		TransactionID: p.TransactionID,
-		OutRefundNo:   p.OutRefundNo,
-		TotalFee:      p.TotalFee,
-		RefundFee:     p.RefundFee,
-		RefundDesc:    p.RefundDesc,
+		AppID:       param["appid"],
+		MchID:       param["mch_id"],
+		NonceStr:    param["nonce_str"],
+		Sign:        sign,
+		SignType:    param["sign_type"],
+		OutRefundNo: param["out_refund_no"],
+		TotalFee:    param["total_fee"],
+		RefundFee:   param["refund_fee"],
+		RefundDesc:  param["refund_desc"],
+		NotifyURL:   param["notify_url"],
 	}
+	if p.OutTradeNo != "" {
+		req.OutTradeNo = p.OutTradeNo
+	}
+	if p.TransactionID != "" {
+		req.TransactionID = p.TransactionID
+	}
+
 	rawRet, err := util.PostXMLWithTLS(refundGateway, req, p.RootCa, refund.MchID)
 	if err != nil {
 		return
@@ -123,4 +119,32 @@ func (refund *Refund) Refund(p *Params) (rsp Response, err error) {
 	}
 	err = fmt.Errorf("[msg : xmlUnmarshalError] [rawReturn : %s] [sign : %s]", string(rawRet), sign)
 	return
+}
+
+// GetSignParam 获取签名的参数
+func (refund *Refund) GetSignParam(p *Params) (param map[string]string) {
+	nonceStr := util.RandomStr(32)
+	param = make(map[string]string)
+	param["appid"] = refund.AppID
+	param["mch_id"] = refund.MchID
+	param["nonce_str"] = nonceStr
+	param["out_refund_no"] = p.OutRefundNo
+	param["refund_desc"] = p.RefundDesc
+	param["refund_fee"] = p.RefundFee
+	param["total_fee"] = p.TotalFee
+
+	if p.SignType == "" {
+		param["sign_type"] = util.SignTypeMD5
+	}
+	if p.OutTradeNo != "" {
+		param["out_trade_no"] = p.OutTradeNo
+	}
+	if p.TransactionID != "" {
+		param["transaction_id"] = p.TransactionID
+	}
+	if p.NotifyURL != "" {
+		param["notify_url"] = p.NotifyURL
+	}
+
+	return param
 }
