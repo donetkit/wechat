fc38dc22c3c96100c5b8752bc1bd61982079a35d
donetkit <donetkit@weinre.cn>
2021-06-19 22:59:14 +0800

    * feat(miniapp): 增加获取微信运动数据接口
    * refactor(werun): 更改werun的位置
    
    * fix(lint): 更改stuct名称

--- miniprogram/miniprogram.go
+++ miniprogram/miniprogram.go
@@ -11,6 +11,7 @@ import (
 	"github.com/donetkit/wechat/miniprogram/qrcode"
 	"github.com/donetkit/wechat/miniprogram/subscribe"
 	"github.com/donetkit/wechat/miniprogram/tcb"
+	"github.com/donetkit/wechat/miniprogram/werun"
 )
 
 //MiniProgram 微信小程序相关API
@@ -71,4 +72,9 @@ func (miniProgram *MiniProgram) GetSubscribe() *subscribe.Subscribe {
 // GetCustomerMessage 客服消息接口
 func (miniProgram *MiniProgram) GetCustomerMessage() *message.Manager {
 	return message.NewCustomerMessageManager(miniProgram.ctx)
+}
+
+// GetWeRun 微信运动接口
+func (miniProgram *MiniProgram) GetWeRun() *werun.WeRun {
+	return werun.NewWeRun(miniProgram.ctx)
 }
