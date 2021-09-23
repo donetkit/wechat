91f289ed5b5dbc99239cedaa8907431a87bb891d
donetkit <donetkit@gmail.cn>
2021-09-23 14:09:00 +0800

    添加无效的open_kfid错误信息

--- work/msgaudit/client_unsupport.go
+++ work/msgaudit/client_unsupport.go
@@ -14,5 +14,5 @@ type Client struct {
 
 // NewClient new
 func NewClient(cfg *config.Config) (*Client, error) {
-	return nil, fmt.Errorf("会话存档功能目前只支持Linux平台运行")
+	return nil, fmt.Errorf("会话存档功能目前只支持Linux平台运行，并且打开设置CGO_ENABLED=1")
 }
