f98b84762c77e6ab03ffc7e9d3f4b108dc92cc9e
donetkit <donetkit@gmail.cn>
2021-11-16 13:17:59 +0800

    TestMemcache

--- cache/memcache_test.go
+++ cache/memcache_test.go
@@ -12,7 +12,7 @@ func TestMemcache(t *testing.T) {
 	mem := NewMemcache("127.0.0.1:11211")
 	var err error
 	timeoutDuration := 10 * time.Second
-	if err = mem.Set("username", "silenceper", timeoutDuration); err != nil {
+	if err = mem.Set("username", "TestMemcache", timeoutDuration); err != nil {
 		t.Error("set Error", err)
 	}
 
