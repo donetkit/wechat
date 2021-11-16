f124907b06c79a17288fe37960d39bc434e14332
donetkit <donetkit@gmail.cn>
2021-11-16 13:16:16 +0800

    微信客服会话变更事件支持查看客服账号ID

--- work/kf/syncmsg/message.go
+++ work/kf/syncmsg/message.go
@@ -142,7 +142,7 @@ type ReceptionistStatusChangeEvent struct {
 	BaseMessage
 	MsgType string `json:"msgtype"` // 消息类型，此时固定为：event
 	Event   struct {
-		EventType          string `json:"event_type"`      // 事件类型。此处固定为：servicer_status_change
+		EventType          string `json:"event_type"`      // 事件类型。 此处固定为：servicer_status_change
 		ReceptionistUserID string `json:"servicer_userid"` // 客服人员userid
 		OpenKFID           string `json:"open_kfid"`       // 客服帐号ID
 		Status             uint32 `json:"status"`          // 状态类型。1-接待中 2-停止接待
