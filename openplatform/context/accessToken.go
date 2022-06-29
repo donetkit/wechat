73e77a5e5702b1e03f098552205a0c8dcd40e37d
donetkit <donetkit@gmail.com>
2022-06-29 14:09:42 +0800

    补充获取授权方帐号基本信息中小程序相关信息

--- openplatform/context/accessToken.go
+++ openplatform/context/accessToken.go
@@ -111,6 +111,91 @@ func (c *Context) GetComponentLoginPage(ctx context.Context, redirectURI string,
 	return componentLoginURLNew, nil
 }
 
+// ID 微信返回接口中各种类型字段
+type ID struct {
+	ID int `json:"id"`
+}
+
+// AuthBaseInfo 授权的基本信息
+type AuthBaseInfo struct {
+	AuthrAccessToken
+	FuncInfo []AuthFuncInfo `json:"func_info"`
+}
+
+// AuthFuncInfo 授权的接口内容
+type AuthFuncInfo struct {
+	FuncscopeCategory ID `json:"funcscope_category"`
+}
+
+// AuthrAccessToken 授权方AccessToken
+type AuthrAccessToken struct {
+	Appid        string `json:"authorizer_appid"`
+	AccessToken  string `json:"authorizer_access_token"`
+	ExpiresIn    int64  `json:"expires_in"`
+	RefreshToken string `json:"authorizer_refresh_token"`
+}
+
+// AuthorizerInfo 授权方详细信息
+type AuthorizerInfo struct {
+	NickName        string `json:"nick_name"`
+	HeadImg         string `json:"head_img"`
+	ServiceTypeInfo ID     `json:"service_type_info"`
+	VerifyTypeInfo  ID     `json:"verify_type_info"`
+	UserName        string `json:"user_name"`
+	PrincipalName   string `json:"principal_name"`
+	BusinessInfo    struct {
+		OpenStore string `json:"open_store"`
+		OpenScan  string `json:"open_scan"`
+		OpenPay   string `json:"open_pay"`
+		OpenCard  string `json:"open_card"`
+		OpenShake string `json:"open_shake"`
+	}
+	Alias     string `json:"alias"`
+	QrcodeURL string `json:"qrcode_url"`
+
+	MiniProgramInfo *MiniProgramInfo       `json:"MiniProgramInfo"`
+	RegisterType    int                    `json:"register_type"`
+	AccountStatus   int                    `json:"account_status"`
+	BasicConfig     *AuthorizerBasicConfig `json:"basic_config"`
+}
+
+// AuthorizerBasicConfig 授权账号的基础配置结构体
+type AuthorizerBasicConfig struct {
+	IsPhoneConfigured bool `json:"isPhoneConfigured"`
+	IsEmailConfigured bool `json:"isEmailConfigured"`
+}
+
+// MiniProgramInfo 授权账号小程序配置 授权账号为小程序时存在
+type MiniProgramInfo struct {
+	Network struct {
+		RequestDomain   []string `json:"RequestDomain"`
+		WsRequestDomain []string `json:"WsRequestDomain"`
+		UploadDomain    []string `json:"UploadDomain"`
+		DownloadDomain  []string `json:"DownloadDomain"`
+		BizDomain       []string `json:"BizDomain"`
+		UDPDomain       []string `json:"UDPDomain"`
+	} `json:"network"`
+	Categories []CategoriesInfo `json:"categories"`
+}
+
+// CategoriesInfo 授权账号小程序配置的类目信息
+type CategoriesInfo struct {
+	First  string `wx:"first"`
+	Second string `wx:"second"`
+}
+
+// ComponentAccessToken 第三方平台
+type ComponentAccessToken struct {
+	util.CommonError
+	AccessToken string `json:"component_access_token"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
+
+type AuthorizerAccessToken struct {
+	AuthorizationInfoExpireTime int64            `json:"authorization_info_expire_time"`
+	AuthorizerAccessToken       AuthrAccessToken `json:"authorizer_info"`
+}
+
 // GetBindComponentURL 获取第三方公众号授权链接(链接跳转，适用移动端)
 func (c *Context) GetBindComponentURL(ctx context.Context, redirectURI string, authType int, bizAppID string) (string, error) {
 	code, err := c.GetPreCode(ctx)
