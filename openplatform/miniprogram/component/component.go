bfe50ed5bc95563dcc3cb8340051f4ebdd883597
DoNetKit <donetkit@gmail.com>
2023-06-07 16:03:29 +0800

    Fix：err != nil return nil

--- openplatform/miniprogram/component/component.go
+++ openplatform/miniprogram/component/component.go
@@ -59,7 +59,7 @@ type GetRegistrationStatusParam struct {
 func (component *Component) GetRegistrationStatus(ctx context.Context, param *GetRegistrationStatusParam) error {
 	componentAK, err := component.GetComponentAccessToken(ctx)
 	if err != nil {
-		return nil
+		return err
 	}
 	url := fmt.Sprintf(fastregisterweappURL+"?action=search&component_access_token=%s", componentAK)
 	data, err := util.PostJSONContext(ctx, url, param)
